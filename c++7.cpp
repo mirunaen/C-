e5eddb33b43f4c6386b7b667e275cc06f386cc44
Miruna <miruna.neds@gmail.com>
2020-05-24 00:57:52 +0200

    Create c++7.cpp

---
+++ c++7.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int num,positive_num=0, num_negative=0,cero;
+  
+  cout<< "Ingresa un num(0 para parar) : ";
+  cin >> num;
+  while(num){
+  	if (num >0)
+  	positive_num++;
+  	else if(num < 0 )
+  	num_negative++;
+  	cout<<"Ingresa otro num (0 para parar): ";
+  	cin >> num;
+  }
+  cout<< "Numeros positivos: "<< positive_num;
+  cout<< " Numeros negativos : "<< num_negative;
+  
+  return 0; 
+	}
+  
+ 
